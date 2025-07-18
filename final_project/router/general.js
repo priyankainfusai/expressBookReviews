0c623acd5ded79c029c0c82cb1086e153da88315
Priyanka <priyanka.bhavsar@infusai.com>
2025-07-18 09:19:57 -0400

    changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,53 +13,85 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(200).json({message: "Books data fetched successfully", books});
+  let promise = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        console.log("adding promise");
+        resolve();
+    }, 1000);
+  })
+  promise.then(data=>{
+    return res.status(200).json({message: "Books data fetched successfully", books});
+  }).catch(err=>{
+    return res.status(500).json({message: "Internam server error"});
+  });
+  
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  let isbn = parseInt(req.params.isbn);
-
-  let filteredBooks = books[isbn]
-  if(filteredBooks){
-      return res.status(200).json({message: "Books based on ISBN",data : filteredBooks});
-  }else{
+  let promise = new Promise((resolve,reject)=>{
+    let isbn = parseInt(req.params.isbn);
+    let filteredBooks = books[isbn]
+    if(filteredBooks){
+        resolve(filteredBooks);
+    }else{
+        reject();
+    }
+  })
+ 
+  promise.then(data=>{
+    return res.status(200).json({message: "Books based on ISBN",data : data});
+  }).catch(err=>{
     return res.status(404).json({message : "No Books found"});
-  }
+  })
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  let author = req.params.author;
-  const booksArray = Object.entries(books).map(([id, book]) => ({
-    id: Number(id),
-    ...book,
-  }));
-  let filteredBooks = booksArray.filter(f=>f.author === author);
-  if(filteredBooks.length > 0){
-
-      return res.status(200).json({message: "Books based on Author",data : filteredBooks});
-  }else{
+  let promise = new Promise((resolve,reject)=>{
+    let author = req.params.author;
+    const booksArray = Object.entries(books).map(([id, book]) => ({
+      id: Number(id),
+      ...book,
+    }));
+    let filteredBooks = booksArray.filter(f=>f.author === author);
+    if(filteredBooks){
+        resolve(filteredBooks)
+    }else{
+        reject();
+    }
+  })
+  
+  promise.then(data=>{
+    return res.status(200).json({message: "Books based on author",data : data});
+  }).catch(err=>{
     return res.status(404).json({message : "No Books found"});
-  }
+  })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     let title = req.params.title;
-    const booksArray = Object.entries(books).map(([id, book]) => ({
-      id: Number(id),
-      ...book,
-    }));
-    let filteredBooks = booksArray.filter(f=>f.title === title);
-    if(filteredBooks.length > 0){
-  
-        return res.status(200).json({message: "Books based on title",data : filteredBooks});
-    }else{
-      return res.status(404).json({message : "No Books found"});
-    }
+    let promise = new Promise((resolve,reject)=>{
+        const booksArray = Object.entries(books).map(([id, book]) => ({
+            id: Number(id),
+            ...book,
+          }));
+          let filteredBooks = booksArray.filter(f=>f.title === title);
+          if(filteredBooks){
+            resolve(filteredBooks)
+        }else{
+            reject();
+        }
+    })
+    
+    promise.then(data=>{
+        return res.status(200).json({message: "Books based on author",data : data});
+      }).catch(err=>{
+        return res.status(404).json({message : "No Books found"});
+      })
 });
 
 //  Get book review
