9e3d34070868c315f58d48d6a56fc2664a68bd15
Priyanka <priyanka.bhavsar@infusai.com>
2025-07-18 07:41:16 -0400

    changes

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,18 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+let name = req.body.name;
+let password = req.body.password;
+
+let accessToken = jwt.sign({
+    data: password
+}, 'access', { expiresIn: 60 * 60 });
+// Store access token and username in session
+req.session.authorization = {
+    accessToken, name
+}
+return res.status(200).send("Customer successfully logged in");
+
 });
  
 const PORT =5000;
@@ -19,4 +31,4 @@ const PORT =5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running on PORT", PORT));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,57 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json({message: "Books data fetched successfully", books});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  const booksArray = Object.entries(books).map(([id, book]) => ({
+    id: Number(id),
+    ...book,
+  }));
+  let filteredBooks = booksArray.filter(f=>f.ISBN === isbn);
+  if(filteredBooks.length > 0){
+
+      return res.status(200).json({message: "Books based on ISBN",data : filteredBooks});
+  }else{
+    return res.status(404).json({message : "No Books found"});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  const booksArray = Object.entries(books).map(([id, book]) => ({
+    id: Number(id),
+    ...book,
+  }));
+  let filteredBooks = booksArray.filter(f=>f.author === author);
+  if(filteredBooks.length > 0){
+
+      return res.status(200).json({message: "Books based on Author",data : filteredBooks});
+  }else{
+    return res.status(404).json({message : "No Books found"});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let title = req.params.title;
+    const booksArray = Object.entries(books).map(([id, book]) => ({
+      id: Number(id),
+      ...book,
+    }));
+    let filteredBooks = booksArray.filter(f=>f.title === title);
+    if(filteredBooks.length > 0){
+  
+        return res.status(200).json({message: "Books based on title",data : filteredBooks});
+    }else{
+      return res.status(404).json({message : "No Books found"});
+    }
 });
 
 //  Get book review
